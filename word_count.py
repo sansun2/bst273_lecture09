d0c379f3f199cb726424d51e4938ab085d9a20ee
Sanjana Sundaresan <ssundaresan@hsph.harvard.edu>
2018-10-04 12:17:20 -0400

    set file path

--- word_count.py
+++ word_count.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
 import argparse
+import sys
 
 parser = argparse.ArgumentParser( description="" )
 
 parser.add_argument(
-	"arg",
-	help="what does this do?",
+	"data_file",
+	help="Path to the file we want to read.",
 )
 
+args = parser.parse_args()
+
+print(args)
+print(args.data_files)
 #-------------------------------------------------------------------------------
 # Are there other arguments we need?
+# Answer: No, we do not need any other arguments.
 #-------------------------------------------------------------------------------
 
 args = parser.parse_args( )
